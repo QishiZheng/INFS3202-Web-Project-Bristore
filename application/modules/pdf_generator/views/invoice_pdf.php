2898ac075d6cb08c2931bfb95fb1b6c325b99192
Qishi Zheng <vincezheng4265@gmail.com>
2018-05-23 16:08:15 +1000

    changed pdf library to tcpdf for invoice pdf file generation;

---
+++ application/modules/pdf_generator/views/invoice_pdf.php
@@ -0,0 +1,16 @@
+<?php
+$pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
+$pdf->SetTitle('Order No.'.$order_id.' Invoice');
+$pdf->SetHeaderMargin(30);
+$pdf->SetTopMargin(20);
+$pdf->setFooterMargin(20);
+$pdf->SetAutoPageBreak(true);
+$pdf->SetAuthor('Bristroe');
+$pdf->SetDisplayMode('real', 'default');
+
+$pdf->AddPage();
+
+$pdf->writeHTML($html, true, false, true, false, 'left');
+$file_name = $order_id."_invoice";
+$path = '/Applications/XAMPP/xamppfiles/htdocs/bristore/invoice_pdf/';
+$pdf->Output($path.$file_name.'.pdf', 'F');
